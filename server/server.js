f24a387b2538940c4280e6f8eeff5a183f11c80b
fahimbavan631@gmail.com <fahimbavan631@gmail.com>
2026-09-19 19:12:55 +0530

    Connect production frontend and backend

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ import researchRoutes from './routes/researchRoutes.js'
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors({ origin: [
-    'http://localhost:5173',
-    'https://researchmind-mnv005jgo-mohammad-fahim-s-projects.vercel.app',
-] }))
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://researchmind-mnv005jgo-mohammad-fahim-s-projects.vercel.app',
+    ]
+}))
 app.use(express.json())
 app.use('/api/research', researchRoutes)
 
